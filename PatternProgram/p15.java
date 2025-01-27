41fd2b7c4fde708e4442a79386432309674f59e8
Someshwar128 <someshwar.waghmode@indiraicem.ac.in>
2025-01-27 18:44:27 +0530

    add p15

---
+++ PatternProgram/p15.java
@@ -0,0 +1,33 @@
+package PatternProgram;
+
+public class p15 {
+    public static void main(String[] args) {
+        f1();
+    }
+
+    public static void f1(){
+        for(int row=1; row<=5; row++){
+            for(int col=1; col<=5; col++){
+                if(col<=row){
+                    System.out.print(" * ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
+/*
+ * 
+ *   *
+ *   * *
+ *   * * *
+ *   * * * *
+ *   * * * * *
+ * 
+ */
